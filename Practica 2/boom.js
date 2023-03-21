155828ba7c950da6c6e39dfd2638956efacd901d
jrburgoa <jr.burgoa.2019@alumnos.urjc.es>
2023-03-21 23:35:20 +0100

    Finished

--- Practica 2/boom.js
+++ Practica 2/boom.js
@@ -3,100 +3,101 @@
 
 //-- Clase cronómetro
 class Crono {
+  constructor(display) {
+    this.display = display;
+    this.cent = 0,
+      this.seg = 0,
+      this.min = 0,
+      this.timer = 0;
+  }
 
-    //-- Constructor. Hay que indicar el 
-    //-- display donde mostrar el cronómetro
-    constructor(display) {
-        this.display = display;
+  tic() {
+    this.cent += 1;
 
-        //-- Tiempo
-        this.cent = 0, //-- Centésimas
-        this.seg = 0,  //-- Segundos
-        this.min = 0,  //-- Minutos
-        this.timer = 0;  //-- Temporizador asociado
+    if (this.cent == 100) {
+      this.seg += 1;
+      this.cent = 0;
     }
 
-    //-- Método que se ejecuta cada centésima
-    tic() {
-        //-- Incrementar en una centesima
-        this.cent += 1;
-
-        //-- 100 centésimas hacen 1 segundo
-        if (this.cent == 100) {
-        this.seg += 1;
-        this.cent = 0;
-        }
-
-        //-- 60 segundos hacen un minuto
-        if (this.seg == 60) {
-        this.min = 1;
-        this.seg = 0;
-        }
-
-        //-- Mostrar el valor actual
-        this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
+    if (this.seg == 60) {
+      this.min = 1;
+      this.seg = 0;
     }
 
-    //-- Arrancar el cronómetro
-    start() {
-       if (!this.timer) {
-          //-- Lanzar el temporizador para que llame 
-          //-- al método tic cada 10ms (una centésima)
-          this.timer = setInterval( () => {
-              this.tic();
-          }, 10);
-        }
+    this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
+  }
+
+  start() {
+    if (!this.timer) {
+      this.timer = setInterval(() => {
+        this.tic();
+      }, 10);
+    }
+  }
+
+  stop() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+  }
+
+  reset() {
+    this.cent = 0;
+    this.seg = 0;
+    this.min = 0;
+
+    this.display.innerHTML = "0:0:0";
+    
+    this.secretkey = [];
+    for (let i = 0; i < 4; i++) {
+      let rnum = getRandomInt(10);
+      this.secretkey.push(rnum);
     }
 
-    //-- Parar el cronómetro
-    stop() {
-        if (this.timer) {
-            clearInterval(this.timer);
-            this.timer = null;
-        }
-    }
+    const line0Buttons = document.querySelectorAll('.line0 .cdigito');
 
-    //-- Reset del cronómetro
-    reset() {
-        this.cent = 0;
-        this.seg = 0;
-        this.min = 0;
-
-        this.display.innerHTML = "0:0:0";
-    }
+    line0Buttons.forEach((button, index) => {
+      button.value = this.secretkey[index];
+      button.textContent = '*';
+    });
+    
+    this.start(); // reiniciar el cronómetro
+  }
 }
 
-//-- Elementos de la gui
 const gui = {
-    display : document.getElementById("display"),
-    start : document.getElementById("start"),
-    stop : document.getElementById("stop"),
-    reset : document.getElementById("reset")
+  display: document.getElementById("display"),
+  start: document.getElementById("start"),
+  stop: document.getElementById("stop"),
+  reset: document.getElementById("reset")
 }
 
 console.log("Ejecuitando JS...");
 
-//-- Definir un objeto cronómetro
 const crono = new Crono(gui.display);
 
-//---- Configurar las funciones de retrollamada
-
-//-- Arranque del cronometro
-gui.start.onclick = () => {
-    console.log("Start!!");
-    crono.start();
+const startCrono = () => {
+  console.log("Start!!");
+  crono.start();
 }
-  
-//-- Detener el cronómetro
+
+gui.start.onclick = startCrono;
+
+const lineButtons = document.querySelectorAll('.line1 button, .line2 button, .line3 button, .line4 button');
+
+lineButtons.forEach((button) => {
+  button.addEventListener('click', startCrono);
+});
+
 gui.stop.onclick = () => {
-    console.log("Stop!");
-    crono.stop();
+  console.log("Stop!");
+  crono.stop();
 }
 
-//-- Reset del cronómetro
 gui.reset.onclick = () => {
-    console.log("Reset!");
-    crono.reset();
+  console.log("Reset!");
+  crono.reset();
 }
 
 // Generar 4 números aleatorios de 0 a 9
@@ -113,16 +114,22 @@ for (let i = 0; i < 4; i++) {
 
 const line0Buttons = document.querySelectorAll('.line0 .cdigito');
 
-
 line0Buttons.forEach((button, index) => {
   button.value = secretkey[index];
   button.textContent = '*';
-
 });
 
-
-// Obtener botones de línea
-const lineButtons = document.querySelectorAll('.line1 button, .line2 button, .line3 button, .line4 button');
+// Función para comprobar si se ha descubierto el código secreto
+function checkCode() {
+  const displayDigits = document.querySelectorAll('.line0 .cdigito');
+  let code = '';
+  displayDigits.forEach((displayDigit) => {
+    code += displayDigit.textContent;
+  });
+  if (code === secretkey.join('')) {
+    crono.stop();
+  }
+}
 
 // Agregar controladores de eventos para cada botón
 lineButtons.forEach((button) => {
@@ -134,8 +141,24 @@ lineButtons.forEach((button) => {
         const displayValue = parseInt(displayDigit.value);
         if (digit === displayValue) {
           displayDigit.textContent = digit;
+          checkCode(); // Comprobar si se ha descubierto el código secreto
         }
       }
     });
   });
 });
+
+function acertarDigito(digito) {
+  const displayDigits = document.querySelectorAll('.line0 .cdigito');
+  displayDigits.forEach((displayDigit) => {
+    if (displayDigit.textContent === '*') {
+      const displayValue = parseInt(displayDigit.value);
+      if (digito === displayValue) {
+        displayDigit.textContent = digito;
+        displayDigit.classList.add('correcto'); // Agregar clase 'correcto'
+        checkCode(); // Comprobar si se ha descubierto el código secreto
+      }
+    }
+  });
+}
+
